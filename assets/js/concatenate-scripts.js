1b9b7a9c3818afc7df4296f2269ceba395ff630c
Bud Parr <budparr@sonnetmedia.net>
2014-04-16 12:43:10 -0400

    update scripts file

--- assets/js/concatenate-scripts.js
+++ assets/js/concatenate-scripts.js
@@ -1,5 +1,8 @@
 ---
 ---
+{% include scripts/jquery.min.js %}
 {% include scripts/widow-control.js %}
 {% include scripts/accordian.js %}
 {% include scripts/nav-top-mobile.js %}
+{% include scripts/smooth-scroll.js %}
+{% include scripts/d3.min.js %}
